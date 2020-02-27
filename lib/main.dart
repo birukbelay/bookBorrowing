7b61fe52e61d0b77873a839b208944c8098d2edb
birukbelay <belay.bura@gmail.com>
2020-02-27 15:03:55 +0300

    list view

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    new MaterialApp(  // this widget is responsible for the whole pa
-      home:new Container(
-        color:Colors.white,
-        child: new Text('hello'),
-      )
+    new MaterialApp(
+      home: new Center(
+        child: ListView(
+          children:<Widget>[
+            Text('one'),
+            Text('one'),
+            Text('one'),
+            Text('one'),
+          ],
+        ),
+      ),
     )
   );
 }
