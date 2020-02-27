3d690d5bf02b60f741ce7812347ac8a0a21c0a58
birukbelay <belay.bura@gmail.com>
2020-02-27 14:40:44 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    new MaterialApp(  // this widget is responsible for the whole page
+    new MaterialApp(  // this widget is responsible for the whole pa
       home:new Container(
         color:Colors.white,
         child: new Text('hello'),
